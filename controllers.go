07afe182fe6bdf17dca19146a0c283991d0c1a22
Sravanth C <54331348+chebro@users.noreply.github.com>
2022-06-10 01:21:09 +0530

    refactor: clean up controllers.go - added utils.go

--- controllers.go
+++ controllers.go
@@ -4,48 +4,29 @@ import (
 	"encoding/json"
 	"html/template"
 	"net/http"
-	"regexp"
 	"sort"
-	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
-	"github.com/guptarohit/asciigraph"
 )
 
 type Device struct {
-	Hostname  string
-	Timestamp int64
-	Graph     string
+	Hostname string
+	Graph    string
 }
 
 func handleGetHome(w http.ResponseWriter, r *http.Request) {
 	var devices []Device
-	var device Device
 	for k := range devicesMap {
-		var g string
-		graph := devicesMap[k].Graph
-		if len(graph) == 0 {
-			g = "no beats yet"
-		} else {
-			diff := (time.Now().Unix() - devicesMap[k].Timestamp) / 60
-			if diff >= 1 {
-				graph = append(graph, make([]float64, diff)...)
-				if len(graph) > 30 {
-					graph = graph[(len(graph) - 30):]
-					graph = append(graph, make([]float64, len(graph)-30)...)
-				}
-			}
-			g = asciigraph.Plot(graph, asciigraph.Precision(0), asciigraph.Height(2))
-			reg := regexp.MustCompile("[0-9]")
-			g = reg.ReplaceAllString(g, "")
-			g = strings.ReplaceAll(g, "  ┼", "")
-			g = strings.ReplaceAll(g, "  ┤", "")
-		}
-		device.Hostname = k
-		device.Timestamp = devicesMap[k].Timestamp
-		device.Graph = g
-		devices = append(devices, device)
+		diff := (time.Now().Unix() - devicesMap[k].Timestamp) / 60
+
+		graph := updateGraph(diff, devicesMap[k].Graph)
+		g := plotGraph(graph)
+
+		devices = append(devices, Device{
+			Hostname: k,
+			Graph:    g,
+		})
 	}
 	sort.Slice(devices, func(i, j int) bool { return devices[i].Hostname < devices[j].Hostname })
 	t, _ := template.ParseFiles("index.html")
@@ -53,36 +34,28 @@ func handleGetHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePostDevices(w http.ResponseWriter, r *http.Request) {
-	var deviceStats DeviceStatsModel
-	err := json.NewDecoder(r.Body).Decode(&deviceStats)
+	var newStats DeviceStatsModel
+
+	err := json.NewDecoder(r.Body).Decode(&newStats)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var prevTimestamp int64
-	currTimestamp := time.Now().Unix()
-	if devicesMap[deviceStats.Hostname].Timestamp == 0 {
-		prevTimestamp = currTimestamp
-	} else {
-		prevTimestamp = devicesMap[deviceStats.Hostname].Timestamp
+
+	id := newStats.Hostname
+
+	oldStats, ok := devicesMap[id]
+	if !ok {
+		oldStats = createDevice(id)
 	}
-	diff := (currTimestamp - prevTimestamp) / 60
-	if len(devicesMap[deviceStats.Hostname].Graph) == 0 {
-		deviceStats.Graph = make([]float64, 30)
-		deviceStats.Graph[29] = 1
-	} else {
-		deviceStats.Graph = devicesMap[deviceStats.Hostname].Graph
-	}
-	if diff >= 1 {
-		deviceStats.Graph = append(deviceStats.Graph, make([]float64, diff)...)
-		deviceStats.Graph = append(deviceStats.Graph, []float64{1}...)
-		if len(deviceStats.Graph) > 30 {
-			deviceStats.Graph = deviceStats.Graph[(len(deviceStats.Graph) - 30):]
-			deviceStats.Graph = append(deviceStats.Graph, make([]float64, len(deviceStats.Graph)-30)...)
-		}
-	}
-	deviceStats.Timestamp = currTimestamp
-	devicesMap[deviceStats.Hostname] = deviceStats
+
+	newStats.Timestamp = time.Now().Unix()
+	diff := (newStats.Timestamp - oldStats.Timestamp) / 60
+	newStats.Graph = updateGraph(diff, oldStats.Graph)
+	newStats.Graph[29] = 1
+
+	devicesMap[id] = newStats
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
---
+++ utils.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/guptarohit/asciigraph"
+)
+
+func createDevice(id string) DeviceStatsModel {
+	devicesMap[id] = DeviceStatsModel{
+		Hostname:  id,
+		Timestamp: time.Now().Unix(),
+		Graph:     make([]float64, 30),
+	}
+	return devicesMap[id]
+}
+
+func updateGraph(diff int64, graph []float64) []float64 {
+	if diff < 30 {
+		graph = append(graph, make([]float64, diff)...)
+		graph = graph[(len(graph) - 30):]
+	} else {
+		graph = make([]float64, 30)
+	}
+	return graph
+}
+
+func plotGraph(graph []float64) string {
+	var g string
+	g = asciigraph.Plot(graph, asciigraph.Precision(0), asciigraph.Height(2))
+	reg := regexp.MustCompile("[0-9]")
+	g = reg.ReplaceAllString(g, "")
+	g = strings.ReplaceAll(g, "  ┼", "")
+	g = strings.ReplaceAll(g, "  ┤", "")
+	return g
+}
